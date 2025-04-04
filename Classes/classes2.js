9ba7716aadfe51aa58450d75f80a846a00dadcab
nafeyJaved <nafeyjaved10@gmail.com>
2025-04-04 20:03:37 +0500

    class 2 linklist example

---
+++ Classes/classes2.js
@@ -0,0 +1,12 @@
+class LinkedList {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
+}
+
+//some other methods: 
+//push, shift, unshift, pop etc 
+
+let myLinkedList = new LinkedList(23)
+myLinkedList.push(7)
