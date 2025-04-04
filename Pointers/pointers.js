1be4508b8e14aa50a67769959bc4e3e505ed20f0
nafeyJaved <nafeyjaved10@gmail.com>
2025-04-04 20:14:04 +0500

    use pointers to point at object value

--- Pointers/pointers.js
+++ Pointers/pointers.js
@@ -6,3 +6,9 @@ num1 = 10
 console.log(num1,num2)
 
 //this is what happens when dont use pointers 
+
+let obj1 = {value: 5}
+let obj2 = obj1
+// obj1.value = 10
+// console.log(obj1.value,obj2.value) 
+//this is what happens when we use pointers
